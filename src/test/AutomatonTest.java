5656c51aece324ee4b65c2a40a302bf649ee776d
cgauna <cgauna@despegar.com>
2015-04-29 19:43:18 -0300

    Arreglado la interseccion mas test

--- src/test/AutomatonTest.java
+++ src/test/AutomatonTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 
 import automaton.Automaton;
 import automaton.BuilderAutomaton;
+import automaton.State;
 
 public class AutomatonTest{
 
@@ -20,10 +21,13 @@ public class AutomatonTest{
 		Set<Character> sigma2 = new HashSet<Character>();
 		sigma2.add('1');
 		sigma2.add('2');
-		sigma2.add('3');
+		sigma2.add('5');
 		Automaton automaton1 = BuilderAutomaton.buildStartAutomaton(sigma1);
 		Automaton automaton2 = BuilderAutomaton.buildStartAutomaton(sigma2);
 		Automaton intersection = Automaton.intersection(automaton1, automaton2);
-		Assert.assertNotNull(intersection);
+		Assert.assertEquals(1, intersection.getStates().length);
+		Assert.assertEquals(intersection.getInitialState(), intersection.transition(intersection.getInitialState(), Character.valueOf('1')));
+		Assert.assertEquals(intersection.getInitialState(), intersection.transition(intersection.getInitialState(), Character.valueOf('2')));
+		Assert.assertNull(intersection.transition(intersection.getInitialState(), Character.valueOf('5')));
 	}
 }
